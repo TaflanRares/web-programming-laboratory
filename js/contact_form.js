298562bbf9f980a13a551f5e97e18a17f6dce69c
TaflanRares <taflan.r@yahoo.com>
2026-02-25 14:55:19 +0200

    Lab2 feat: improved contact form check

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,7 +1,36 @@
+function notifTrimitere(message) {
+    const feedbackElement = document.querySelector("#form-feedback");
+    feedbackElement.textContent = message;
+}
+
 function submitForm() {
-    const nume = document.getElementById("name").value;
+    const name = document.getElementById("name").value;
     const mail = document.getElementById("email").value;
     const mesaj = document.getElementById("message").value;
-    console.log(`Nume: ${nume}\nEmail: ${mail}\nMesaj: ${mesaj}`);
-    console.warn("Functia submitForm a fost apelata");
+
+    const feedbackElement = document.querySelector("#form-feedback");
+    feedbackElement.style.color = "red";
+
+    if (!mail.includes("@") || !mail.includes(".")) {
+        notifTrimitere("Email not valid!");
+        return;
+    }
+    if (mesaj.length < 10) {
+        notifTrimitere("Message must be at least 10 characters long!");
+        return;
+    }
+    if (name.trim().length < 2) {
+        notifTrimitere("Name must be at least 2 characters long!");
+        return;
+    }
+
+    feedbackElement.style.color = "green";
+    notifTrimitere(`Thank you ${name}, I will contact you soon!`);
+    console.log(`Name: ${name}\nEmail: ${mail}\nMesaj: ${mesaj}`);
 }
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitForm();
+});
